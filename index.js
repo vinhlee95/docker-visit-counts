f409a1f20c8bf7c4eec8146ae365fe0d5017305d
Vinh Le <vinh@vertics.co>
2019-03-17 11:23:01 +0200

    Add docker compose yml

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const redis = require('redis')
 
 const app = express()
 
-const client = redis.createClient()
+// connect to redis-server container
+const client = redis.createClient({
+	host: 'redis-server',
+	port: 6379
+})
 client.set('visits', 0)
 
 app.get('/', (req, res) => {
